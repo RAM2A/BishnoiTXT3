31fe3d94b551feb56ccfe455d63381b070c96c81
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-05-14 20:45:14 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-api_id = "23881274"
-api_hash = "c867262073a58ccdebcae19601c5bf5f"
-bot_token = "7173171084:AAEOkdr8x7LFW0Q31GdNvGzeMt2gDA1g3YY"
-auth_groups = "-1001928293542"
+api_id = "24665357"
+api_hash = "beb7e4b83ada668fa85f9a9b56338f1d"
+bot_token = "6700540841:AAEzEG75XEQXqfTGmIvy136zVclAUBxQKOI"
+auth_groups = "-1002137417670"
